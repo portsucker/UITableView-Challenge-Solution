0bcc661420acce0d12b6005c70a3427528310c59
portsucker <kush@Kushs-MacBook-Pro.local>
2015-01-07 11:39:56 +0300

    final proj

---
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Eliot Arntz on 10/19/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
